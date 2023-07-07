0a5131aeb1fcd64b93062539996d9f7ef4b2e87d
Levon Gabrielyan <levongabrielyan44@gmail.com>
2023-07-07 16:30:13 +0400

    Add project files.

---
+++ Day_3_DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿class Tasks
+{
+    public static string ReplaceWithDate(string arg)//Task 1
+    {
+        return arg.Replace("@expDate@",DateTime.Now.AddMonths(1).ToString());
+    }
+    public static string LastMilisecond(string arg)//Task 2
+    {
+        return arg.Replace("@expDate@",new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddMilliseconds(-1).ToString("yyyy-MM-dd HH:mm:ss.fff"));
+    }
+
+}
+
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
